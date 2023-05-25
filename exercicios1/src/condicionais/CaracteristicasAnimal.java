333a2fddb063daa2c7ce387b046982bd1e37bc38
Igor_Augusto <feitosa66@gmail.com>
2023-05-25 15:53:40 -0300

    exercicios condicionais

---
+++ exercicios1/src/condicionais/CaracteristicasAnimal.java
@@ -0,0 +1,57 @@
+package condicionais;
+
+import java.util.Scanner;
+
+public class CaracteristicasAnimal {
+
+	public static void main(String[] args) {
+		String primeiraCarac, segundaCarac, terceiraCarac;
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Vamos advinhar um animal com base em suas caracteristicas!");
+		System.out.println("\nInforme os dados nos campos abaixo!");
+		
+		System.out.println("Primeira caracteristica: ");
+		primeiraCarac = leia.nextLine();
+		System.out.println("Segunda caracteristica: ");
+		segundaCarac = leia.nextLine();
+		System.out.println("Terceira caracteristica: ");
+		terceiraCarac = leia.nextLine();
+		
+		if(primeiraCarac.equalsIgnoreCase("vertebrado")) {
+			if(segundaCarac.equalsIgnoreCase("ave")) {
+				if(terceiraCarac.equalsIgnoreCase("carnivoro")) {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Águia!");
+				}else {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Pomba!");
+				}
+				
+			}else if(segundaCarac.equalsIgnoreCase("mamifero")) {
+				if(terceiraCarac.equalsIgnoreCase("onivoro")) {
+					System.out.println("Com base nessas caracteristicas, o animal é um Homem!");
+				}else {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Vaca!");
+				}
+			}
+			
+		}else if(primeiraCarac.equalsIgnoreCase("invertebrado")) {
+			if(segundaCarac.equalsIgnoreCase("inseto")) {
+				if(terceiraCarac.equalsIgnoreCase("hematofago")) {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Pulga!");
+				}else {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Lagarta!");
+				}
+				
+			}else if(segundaCarac.equalsIgnoreCase("anelideo")) {
+				if(terceiraCarac.equalsIgnoreCase("hematofago")) {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Sanguessuga!");
+				}else {
+					System.out.println("Com base nessas caracteristicas, o animal é uma Minhoca!");
+				}
+			}
+			
+		}
+		
+	}
+
+}
---
+++ exercicios1/src/condicionais/DoacaoDeSangue.java
@@ -0,0 +1,46 @@
+package condicionais;
+
+import java.util.Scanner;
+
+public class DoacaoDeSangue {
+
+	public static void main(String[] args) {
+		int idade;
+		String nome, primeiraDoacao;
+
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Bem- vindo ao Doação de Sangue!");
+		System.out.println("Vamos verificar se você está apto para doar sangue.");
+		System.out.println("Por favor informe seus dados nos campos abaixo:");
+
+		System.out.print("Qual é seu nome: ");
+		nome = leia.nextLine();
+
+		System.out.print("Qual sua idade: ");
+		idade = leia.nextInt();
+
+		if (idade < 18 || idade > 69) {
+			System.out.println(nome + ", não está apto(a) para doar sangue!");
+		}
+		
+		else if (idade >= 60 && idade <= 69) {
+			System.out.println("É a sua primeira vez doando sangue? [sim/não]");
+			primeiraDoacao = leia.next();
+			
+			
+			if (primeiraDoacao.equalsIgnoreCase("Sim")) {
+				System.out.println(nome + ", não está apto(a) para doar sangue!");
+			} else {
+				System.out.println(nome + ", está apto(a) para doar sangue!");
+			}
+		} 
+		
+		else if (idade >= 18 && idade <= 59) {
+			System.out.println(nome + ", está apto(a) para doar sangue!");
+		} 
+		
+
+	}
+
+}
