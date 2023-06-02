28d2fedb929bf10a8b9f6ec84327199375b444ba
augustofeitosa <feitosa66@gmail.com>
2023-06-02 19:59:25 -0300

    first commit

--- exercicios1/src/lojaDeGames/Produtos.java
+++ exercicios1/src/lojaDeGames/Produtos.java
@@ -1,7 +1,5 @@
 package lojaDeGames;
 
-import lojaDeGames.CadastroGames;
-
 public class Produtos{
     public static void main(String[] args) {
         CadastroGames jogoUm = new CadastroGames("The Last of Us II", 2020, "Playstation 4", "Ação e Aventura", 250.00, 10);
